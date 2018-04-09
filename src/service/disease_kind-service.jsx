e018b6225a75bfe9f1537166b57e2538b4bf9ad1
李桐 <lt@litongdeMacBook-Pro.local>
2018-04-09 14:18:39 +0800

    update code

--- src/service/disease_kind-service.jsx
+++ src/service/disease_kind-service.jsx
@@ -16,7 +16,7 @@ class DiseaseK{
             url                        ='http://192.168.1.101:5188/dikind/allList?pageSize=10&curPage=' + (listParam.curPage*1 - 1);
             //data.curPage               =listParam.curPage;
         }else if (listParam.listType ==='search'){
-            url                        ='http://192.168.1.101:5188/dikind/allList/%E5%86%85%E7%A7%91%E7%97%85';
+            url                        ='http://192.168.1.101:5188/dikind/allList/%E7%94%B7%E7%A7%91%E7%97%85';
             data.curPage               =listParam.curPage;
            // data[listParam.searchType] =listParam.keyword;
         }
